987ea00b91dd13de790ad9dd7bb8de7ebe6f5bd8
Jason W Brown <jasonwbrown23@gmail.com>
2023-03-18 17:04:22 -0700

    update create skeleton script

--- create-skeleton.sh
+++ create-skeleton.sh
@@ -6,7 +6,7 @@ mkdir -p $1 || echo "assuming $1 exists"
 # create files
 touch $1/main.go $1/main_test.go $1/Makefile & \
     echo "package main" > $1/main.go & \
-    echo "func main() {}" > $1/main.go & \
+    echo "func main() {}" >> $1/main.go & \
     echo "package main" > $1/main_test.go
 
 
