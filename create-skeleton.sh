57b9f7f7d58f7e77569f788e43b99798db86aa07
Jason W Brown <jasonwbrown23@gmail.com>
2023-03-19 13:03:21 -0700

    remove makefile from create script

--- create-skeleton.sh
+++ create-skeleton.sh
@@ -4,7 +4,7 @@
 echo "will create directory $1"
 mkdir -p $1 || echo "assuming $1 exists"
 # create files
-touch $1/main.go $1/main_test.go $1/Makefile & \
+touch $1/main.go $1/main_test.go & \
     echo "package main" > $1/main.go & \
     echo "func main() {}" >> $1/main.go & \
     echo "package main" > $1/main_test.go
